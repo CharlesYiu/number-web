e9f375f91177a2240716a34709e22c1c92e5d055
Charles Yiu <charlesyiu.w@gmail.com>
2021-12-07 18:02:06 +0800

    changes

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # number-web
-Web version of charlesyiu/number-app 
+The simplified html version of [charlesyiu/number-app](https://github.com/charlesyiu/number-app)
+# Controls
+## Left Click
+Adds 1
+## Right Click
+Subtracts 1
+## Hold Space Key
+### Mouse in the top half of the webpage
+Adds 1 repeatedly   
+(When the number is negative it will subtract instead)
+### Mouse in the bottom half of the webpage
+Subtracts 1 repeatedly   
+(When the number is negative it will add instead)
+### Mouse in the left half of the webpage
+Changes the number to negative
+### Mouse in the right half of the webpage
+Changes the number to positive
+## Press R Key
+Resets the number to 0
---
+++ script.js
@@ -0,0 +1,68 @@
+const NumberLabel = document.getElementById("number");
+let NumberValue = 0;
+function UpdateNumberLabel() {
+    NumberLabel.textContent = NumberValue.toString();
+}
+// on left click
+window.onclick = function() {
+    NumberValue += 1;
+    UpdateNumberLabel();
+}
+// on right click
+window.oncontextmenu = function() {
+    NumberValue -= 1;
+    UpdateNumberLabel();
+    return false;
+}
+let RunningSpaceLoop = false;
+let AddNumber = true;
+let NegativeNumber = false;
+let LastNegativeNumber = true;
+let InvertAddNumber = false;
+function SpaceLoop() {
+    let InvertedAddNumber = AddNumber;
+    if (InvertAddNumber) {
+        InvertedAddNumber = !InvertedAddNumber;
+    }
+    if (InvertedAddNumber) {
+        NumberValue += 1;
+    } else {
+        NumberValue -= 1;
+    }
+    UpdateNumberLabel();
+    if (RunningSpaceLoop) {
+        setTimeout(SpaceLoop, 100);
+    }
+}
+window.onkeydown = function(event) {
+    if (event.keyCode === 32 && !RunningSpaceLoop) {
+        // start space loop if not running space loop
+        InvertAddNumber = NumberValue < 0;
+        RunningSpaceLoop = true;
+        SpaceLoop();
+    // reset
+    } else if (event.keyCode === 82) {
+        NumberValue = 0;
+        UpdateNumberLabel();
+    }
+};
+window.onkeyup = function(event) {
+    if (event.keyCode === 32) {
+        RunningSpaceLoop = false;
+    }
+};
+window.onmousemove = function(event) {
+    // negative number
+    NegativeNumber = event.clientX < (NumberLabel.clientWidth / 2);
+    if (NegativeNumber !== LastNegativeNumber) {
+        if (NegativeNumber) {
+            NumberValue = -Math.abs(NumberValue);
+        } else {
+            NumberValue = Math.abs(NumberValue);
+        }
+        InvertAddNumber = NegativeNumber;
+        LastNegativeNumber = NegativeNumber;
+    }
+    // add number
+    AddNumber = event.clientY < (NumberLabel.clientHeight / 2);
+};
